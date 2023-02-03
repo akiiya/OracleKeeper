57ab1c1d919540d544cb8ce0345e6c6040ec1cd0
neno <healneno@gmail.com>
2023-02-03 17:56:17 +0800

    update

--- oracle_keeper.sh
+++ oracle_keeper.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-
-checkINSTALL() {
-  if [ -f /usr/local/oracle_keeper/oracle_keeper.py ]; then
+check() {
+  if [ -f /usr/lib/systemd/system/oracle_keeper.service ]; then
     return 1
   else
     return 0
@@ -10,6 +9,11 @@ checkINSTALL() {
 }
 
 install() {
+  check
+  if [[ $? == 1 ]]; then
+    uninstall
+  fi
+
   mkdir -p /usr/local/oracle_keeper
   wget -N --no-check-certificate https://raw.githubusercontent.com/akiiya/OracleKeeper/master/oracle_keeper.py -O /usr/local/oracle_keeper/oracle_keeper.py
   wget -N --no-check-certificate https://raw.githubusercontent.com/akiiya/OracleKeeper/master/oracle_keeper.service -O /usr/lib/systemd/system/oracle_keeper.service
@@ -20,9 +24,14 @@ install() {
 }
 
 update() {
-  wget -N --no-check-certificate https://raw.githubusercontent.com/akiiya/OracleKeeper/master/oracle_keeper.py -O /usr/local/oracle_keeper/oracle_keeper.py
-  systemctl restart oracle_keeper
-  echo '服务更新完成'
+  check
+  if [[ $? == 0 ]]; then
+    install
+  else
+    wget -N --no-check-certificate https://raw.githubusercontent.com/akiiya/OracleKeeper/master/oracle_keeper.py -O /usr/local/oracle_keeper/oracle_keeper.py
+    systemctl restart oracle_keeper
+    echo '服务更新完成'
+  fi
 }
 
 uninstall() {
@@ -35,29 +44,12 @@ uninstall() {
   echo '服务卸载完成'
 }
 
-RunScript() {
-  if [ "$1" = "install" ]; then
-    if [ "$check" ]; then
-      uninstall
-      install
-    else
-      install
-    fi
-  elif [ "$1" = "update" ]; then
-    if [ "$check" ]; then
-      update
-    else
-      install
-    fi
-  elif [ "$1" = "uninstall" ]; then
-    uninstall
-  else
-    echo "Error command"
-  fi
-}
-
-RunScript $1
-
-echo '脚本执行完成'
-
-
+if [ "$1" = "install" ]; then
+  install
+elif [ "$1" = "update" ]; then
+  update
+elif [ "$1" = "uninstall" ]; then
+  uninstall
+else
+  echo "Error command"
+fi
