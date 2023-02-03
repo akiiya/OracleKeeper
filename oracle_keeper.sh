76e30b7c18734a1e67677fc15e26345b08388f02
neno <healneno@gmail.com>
2023-02-03 17:24:08 +0800

    update

--- oracle_keeper.sh
+++ oracle_keeper.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+py_path="/usr/local/oracle_keeper/oracle_keeper.py"
+
+check() {
+  if [ -f /usr/local/oracle_keeper/oracle_keeper.py ]; then
+    return 1
+  else
+    return 0
+  fi
+}
+
 install() {
   mkdir -p /usr/local/oracle_keeper
   wget -N --no-check-certificate https://raw.githubusercontent.com/akiiya/OracleKeeper/master/oracle_keeper.py -O /usr/local/oracle_keeper/oracle_keeper.py
@@ -24,10 +34,21 @@ uninstall() {
 }
 
 RunScript() {
+  check_result=$(check)
+
   if [ "$1" = "install" ]; then
-    install
+    if [ check_result ]; then
+      uninstall
+      install
+    else
+      install
+    fi
   elif [ "$1" = "update" ]; then
-    update
+    if [ check_result ]; then
+      update
+    else
+      install
+    fi
   elif [ "$1" = "uninstall" ]; then
     uninstall
   else
