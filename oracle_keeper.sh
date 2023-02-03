03efa3847aee531f5ea32e2142f1dd3470102b91
neno <healneno@gmail.com>
2023-02-03 14:09:41 +0800

    update script

--- oracle_keeper.sh
+++ oracle_keeper.sh
@@ -2,15 +2,15 @@
 
 install() {
   mkdir -p /usr/local/oracle_keeper
-  wget -N --no-check-certificate https://raw.githubusercontent.com/OracleKeeper/master/oracle_keeper.py -O /usr/local/oracle_keeper/oracle_keeper.py
-  wget -N --no-check-certificate https://raw.githubusercontent.com/OracleKeeper/master/oracle_keeper.service -O /usr/lib/systemd/system/oracle_keeper.service
+  wget -N --no-check-certificate https://raw.githubusercontent.com/akiiya/OracleKeeper/master/oracle_keeper.py -O /usr/local/oracle_keeper/oracle_keeper.py
+  wget -N --no-check-certificate https://raw.githubusercontent.com/akiiya/OracleKeeper/master/oracle_keeper.service -O /usr/lib/systemd/system/oracle_keeper.service
   systemctl daemon-reload
   systemctl enable oracle_keeper
   systemctl start oracle_keeper
 }
 
 update() {
-  wget -N --no-check-certificate https://raw.githubusercontent.com/OracleKeeper/master/oracle_keeper.py -O /usr/local/oracle_keeper/oracle_keeper.py
+  wget -N --no-check-certificate https://raw.githubusercontent.com/akiiya/OracleKeeper/master/oracle_keeper.py -O /usr/local/oracle_keeper/oracle_keeper.py
   systemctl restart oracle_keeper
 }
 
